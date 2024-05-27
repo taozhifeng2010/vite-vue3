cfaaf9fcc17779be30e1e210ed8bc83ffec94f99
taozhifeng2010 <taozhifeng2010@163.com>
2024-05-27 16:14:11 +0800

    代码初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ name }}</div>
+  <div>{{ age }}</div>
+  <div>{{ tel }}</div>
+  <button @click="nameFn">修改名字</button>
+  <button @click="ageFn">修改年龄</button>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+
+let name: Ref<string> = ref('张杰');
+let age = ref(18);
+let tel = ref('18710582218');
+
+function nameFn() {
+  name.value = 'zhangjie';
+  console.log('222', name);
+
+};
+
+function ageFn() {
+  age.value += 1
+};
+
+</script>
+
+<style lang="scss" scoped></style>
